8708b88a168a783559b11ca325fb2771f49a77b5
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-03-23 09:20:27 +0800

    Updated comments

--- lrucache.go
+++ lrucache.go
@@ -8,13 +8,11 @@ type cache_container struct {
 	values []string
 }
 
-// Executes the proess of generating the LRU cache
+// GenerateLRUCache generates the output of the LRU cache
 func GenerateLRUCache(input string) string {
 
 	cache := cache_container{}
 	cachedVals := &cache.values
-
-
 
 	for _, char := range input {
 		valueToCheck := string(char)
@@ -23,7 +21,7 @@ func GenerateLRUCache(input string) string {
 		if inArray == false {
 			*cachedVals = append(*cachedVals, valueToCheck)
 		} else {
-			
+
 			updateCollecton(&cache, valueToCheck)
 		}
 	}
@@ -34,6 +32,7 @@ func GenerateLRUCache(input string) string {
 	return "you are done"
 }
 
+// in_array Checks if a paritcular element is found in a collection
 func in_array(val string, array []string) (ok bool, i int) {
 	for i = range array {
 		if ok = array[i] == val; ok {
@@ -43,17 +42,16 @@ func in_array(val string, array []string) (ok bool, i int) {
 	return
 }
 
+// updateCollection updates the cache container collection trough reference. This is where the main logic is
 func updateCollecton(cache *cache_container, letter string) {
 
 	for index, char := range cache.values {
 		if string(char) == letter {
-			// Removing the cached letter 
-			fmt.Println("Before : ", cache)
+			// Removing the cached letter
 			cache.values = append(cache.values[:index], cache.values[index+1:]...)
 
 			// adding to the end
 			cache.values = append(cache.values, letter)
-			fmt.Println("after: ", cache)
 		}
 	}
 }
