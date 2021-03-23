4e5afcb59601b1c91ab4d4a3599253794e69035b
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-03-23 09:18:10 +0800

    Corrected the implementation

--- lrucache.go
+++ lrucache.go
@@ -4,46 +4,56 @@ import (
 	"fmt"
 )
 
+type cache_container struct {
+	values []string
+}
+
 // Executes the proess of generating the LRU cache
 func GenerateLRUCache(input string) string {
 
-	theCache := []string{}
+	cache := cache_container{}
+	cachedVals := &cache.values
 
-	for _, value := range input {
-		char := string(value)
-		inArray, _ := in_array(char, theCache)
+
+
+	for _, char := range input {
+		valueToCheck := string(char)
+		inArray, _ := in_array(valueToCheck, *cachedVals)
 
 		if inArray == false {
-			theCache = append(theCache, string(char))
+			*cachedVals = append(*cachedVals, valueToCheck)
 		} else {
-			theCache = updateCollecton(theCache, value)
+			
+			updateCollecton(&cache, valueToCheck)
 		}
 	}
 
-	fmt.Println("final", theCache)
+	fmt.Println("Final: ")
+	fmt.Println(cache)
 
 	return "you are done"
 }
 
 func in_array(val string, array []string) (ok bool, i int) {
 	for i = range array {
-			if ok = array[i] == val; ok {
-					return
-			}
+		if ok = array[i] == val; ok {
+			return
+		}
 	}
 	return
 }
 
-func updateCollecton(theCache []string, letter rune) []string {
+func updateCollecton(cache *cache_container, letter string) {
 
-	for index, char := range theCache {
-		fmt.Println("removing " + string(letter) + " from ...")1
-		fmt.Println(theCache)
-		if string(letter) == string(char) {
-			theCache = append(theCache[:index], theCache[index+1:]...)
+	for index, char := range cache.values {
+		if string(char) == letter {
+			// Removing the cached letter 
+			fmt.Println("Before : ", cache)
+			cache.values = append(cache.values[:index], cache.values[index+1:]...)
+
+			// adding to the end
+			cache.values = append(cache.values, letter)
+			fmt.Println("after: ", cache)
 		}
 	}
-
-	return theCache
-
 }
